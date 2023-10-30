473ffa03441170041675ac7283cae4d153b71e14
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 10:26:44 +0100

    Add scanner

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,7 +1,13 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //Istanzio lo scanner
+        Scanner scan = new Scanner(System.in);
 
+        //Chiudo lo scanner
+        scan.close();
     }
 }
