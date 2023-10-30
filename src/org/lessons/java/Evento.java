89e7d7ca077970c4fe0c85eb1495b9ca25b5cc2c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 09:44:43 +0100

    Add attributes and constant

--- src/org/lessons/java/Evento.java
+++ src/org/lessons/java/Evento.java
@@ -1,4 +1,13 @@
 package org.lessons.java;
 
+import java.util.Date;
+
 public class Evento {
+    //COSTANTI DI CLASSI
+    public final static int MAX_POSTI = 3000;
+
+    //Attributi
+    private String titolo;
+    private Date data;
+    private int postiPrenotati;
 }
