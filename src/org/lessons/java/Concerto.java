87992423137c8e1a782795bd96e1f94c9f3be172
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 14:08:24 +0100

    add attributes

--- src/org/lessons/java/Concerto.java
+++ src/org/lessons/java/Concerto.java
@@ -1,8 +1,15 @@
 package org.lessons.java;
 
+import java.math.BigDecimal;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class Concerto extends Evento{
+    //attributi
+    private LocalTime ora;
+    private BigDecimal prezzo;
+
+    //Costruttore
     public Concerto(String titolo, Date data, int postiTotali) throws IllegalArgumentException {
         super(titolo, data, postiTotali);
     }
