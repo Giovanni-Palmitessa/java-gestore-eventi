f92809d35429e3aa267f259a30b04907a303909a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 10:29:37 +0100

    Add title variable

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -7,6 +7,12 @@ public class Main {
         //Istanzio lo scanner
         Scanner scan = new Scanner(System.in);
 
+        //Chiedo all'utente di aggiungere un nuovo Evento
+        System.out.println("Gestore di Eventi, Benvenuto! Aggiungi un nuovo evento!");
+        //titolo
+        System.out.println("Inserisci il titolo dell'Evento: ");
+        String titolo = scan.nextLine();
+
         //Chiudo lo scanner
         scan.close();
     }
