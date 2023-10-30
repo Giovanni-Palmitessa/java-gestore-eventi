4277668df79dce1e83f47c9f3487f2360ce2981b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 15:08:51 +0100

    Add constructor

--- src/org/lessons/java/ProgrammEventi.java
+++ src/org/lessons/java/ProgrammEventi.java
@@ -1,9 +1,17 @@
 package org.lessons.java;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProgrammEventi {
     //Attributi
     private String titolo;
     private List<Evento> listaEventi;
+
+    //Costruttore
+
+    public ProgrammEventi(String titolo) {
+        this.titolo = titolo;
+        this.listaEventi = new ArrayList<>();
+    }
 }
