7b587f1ab90b395c43bdfa8d32906836088d4440
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-30 15:26:47 +0100

    add first 3 methods

--- src/org/lessons/java/ProgrammEventi.java
+++ src/org/lessons/java/ProgrammEventi.java
@@ -1,6 +1,7 @@
 package org.lessons.java;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ProgrammEventi {
@@ -9,9 +10,27 @@ public class ProgrammEventi {
     private List<Evento> listaEventi;
 
     //Costruttore
-
     public ProgrammEventi(String titolo) {
         this.titolo = titolo;
         this.listaEventi = new ArrayList<>();
+    }
+    //Metodi
+    //aggiungere evento a lista Eventi
+    public void aggiungiEvento(Evento evento) {
+        listaEventi.add(evento);
+    }
+    // ritornare eventi di una determinata data
+    public List<Evento> eventiData(Date data) {
+        List<Evento> eventiData = new ArrayList<>();
+        for (Evento evento : listaEventi) {
+            if (evento.getData().equals(data)) {
+                eventiData.add(evento);
+            }
+        }
+        return eventiData;
+    }
+    // contare eventi presenti nella lista
+    public int numeroEventi() {
+        return listaEventi.size();
     }
 }
